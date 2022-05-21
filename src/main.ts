cba1c0ca1cdf9ffaf8ea6ec66a892c94aee7cff3
Tomasz Paczka <tpaczka@future-processing.com>
2022-05-21 23:44:35 +0200

    Trying on aws with express.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-
 bootstrap();
